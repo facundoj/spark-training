16f00723ac40d60e13875306aee100be7ca72ee0
facundoj <jaureguifacundo@gmail.com>
2015-11-19 17:56:57 -0300

    Log parser

--- lab2.py
+++ lab2.py
@@ -1,10 +1,41 @@
 from pyspark import SparkContext
 
+import re
+
+
+def parse_apache_time(time):
+    # todo: implement
+    return time
+
+
+class LogLine(object):
+    def __init__(self, host, availability, userid, time, method, resource, protocol, status_code, size):
+        self.host = host
+        self.client_ident = availability
+        self.user_id = userid
+        self.time = parse_apache_time(time)
+        self.method = method
+        self.resource = resource
+        self.protocol = protocol
+        self.status_code = status_code
+        self.size = size
+
+# http://httpd.apache.org/docs/1.3/logs.html#common
+# LogFormat "%h %l %u %t \"%r\" %>s %b" common
+LOG_PATTERN = r'(\S+) (\S+) (\S+) \[(.+)\] "(\S+) (\S+) (\S+)" (\d+) (\d+)'
+
+
+def parseLogLine(line):
+    res = re.search(LOG_PATTERN, line)
+
+    if not res:
+        return
+    else:
+        info = []
+        for i in range(1, 10):
+            info.append(res.group(i))
+
+        return LogLine(*info)
+
 sc = SparkContext('local', 'Lab 2')
 
-
-# 199.72.81.55 - - [01/Jul/1995:00:00:01 -0400] "GET /history/apollo/ HTTP/1.0" 200 6245
-# unicomp6.unicomp.net - - [01/Jul/1995:00:00:06 -0400] "GET /shuttle/countdown/ HTTP/1.0" 200 3985
-# 199.120.110.21 - - [01/Jul/1995:00:00:09 -0400] "GET /shuttle/missions/sts-73/mission-sts-73.html HTTP/1.0" 200 4085
-
-LOG_PATTERN = r'(\S+) (\S+) (\S+) \[(.+)\] "(\S+) (\S+) (\S+)" (\d+) (\d+)'
